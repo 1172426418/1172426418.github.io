da9ddeb902292149b255124c9e620adfedee63c7
fate <1172426418@qq.com>
2021-11-26 17:52:50 +0800

    新增播放器开关配置

--- themes/ayer/source/dist/music.js
+++ themes/ayer/source/dist/music.js
@@ -1,3 +1,7 @@
+<link rel="stylesheet" href="/dist/APlayer.min.css">
+<div id="aplayer"></div>
+<script type="text/javascript" src="/dist/APlayer.min.js"></script>
+<script type="text/javascript" src="/dist/music.js"></script>
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     fixed: true,
