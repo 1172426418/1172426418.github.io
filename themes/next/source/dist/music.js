b8cd134a2f23baf92493d20e09b9462236eb9e62
fatechen <fatechen@kaiser.com.cn>
2022-04-21 18:33:23 +0800

    添加next主题

---
+++ themes/next/source/dist/music.js
@@ -0,0 +1,32 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "花岡拓也、鶴山尚史 - 王の愛は民のために",
+        artist: '花岡拓也、鶴山尚史',
+        url: '/mp3/花岡拓也、鶴山尚史 - 王の愛は民のために.mp3',
+        cover: 'https://z3.ax1x.com/2021/11/26/oV3kM8.jpg',
+      },
+        {
+            name: 'HEATS',
+            artist: 'V.A.',
+            url: '/mp3/HEATS.mp3',
+            cover: 'https://z3.ax1x.com/2021/11/26/oVt0bT.jpg',
+        },
+      {
+        name: 'UNTRUE CRYSTAL',
+        artist: '花岡拓也、鶴山尚史',
+        url: '/mp3/UNTRUE CRYSTAL.mp3',
+        cover: 'https://z3.ax1x.com/2021/11/26/oVt2x1.jpg',
+      },
+        {
+            name: '天の金剛',
+            artist: '花岡拓也、鶴山尚史',
+            url: '/mp3/天の金剛.mp3',
+            cover: 'https://s3.bmp.ovh/imgs/2021/11/c117848e5ac3403e.jpg',
+        },
+
+    ]
+});
